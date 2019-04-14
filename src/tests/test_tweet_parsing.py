51392dc2b99125cca37641b76e9f5c218d15bd0c
Joseph Flanagan <joseph.flanagan@helsinki.fi>
2019-04-14 11:21:58 +0300

    Make explicit test is testing empty elements

--- src/tests/test_tweet_parsing.py
+++ src/tests/test_tweet_parsing.py
@@ -31,31 +31,31 @@ def test_place(tweet):
     assert parse_tweets.place(tweet) == 'Boulder, CO'
 
 
-def test_hashtags(tweet):
+def test_empty_hashtags(tweet):
     assert parse_tweets.hashtags(tweet) == ''
 
 
-def test_all_hastags(tweet):
-    assert parse_tweets.all_hashtags(tweet) == ''
+def test_empty_quoted_or_retweeted_hastags(tweet):
+    assert parse_tweets.quoted_or_retweeted_hashtags(tweet) == ''
 
 
 def test_media(tweet):
     assert parse_tweets.media(tweet) == media
 
 
-def test_urls(tweet):
+def test_empty_urls(tweet):
     assert parse_tweets.urls(tweet) == ''
 
 
-def test_retweet_id(tweet):
+def test_empty_retweet_id(tweet):
     assert parse_tweets.retweet_id(tweet) == ''
 
 
-def test_retweet_screen_name(tweet):
+def test_empty_retweet_screen_name(tweet):
     assert parse_tweets.retweet_screen_name(tweet) == ''
 
 
-def test_retweet_user_id(tweet):
+def test_empty_retweet_user_id(tweet):
     assert parse_tweets.retweet_user_id(tweet) == ''
 
 
@@ -63,7 +63,7 @@ def test_tweet_url(tweet):
     assert parse_tweets.tweet_url(tweet) == tweet_url
 
 
-def test_user_urls(tweet):
+def test_empty_user_urls(tweet):
     assert parse_tweets.user_urls(tweet) == ''
 
 
